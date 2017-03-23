b1f28e82a0a9703bffd44153a24d80f12c19652b
Ollie Warren <oliverwarren1@hotmail.com>
2017-03-23 14:06:26 +0000

    Cocktails controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,38 @@
+class CocktailsController < ApplicationController
+  before_action :load_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def load_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ApplicationRecord
+  has_many :cocktails, through: :doses
+  has_many :doses
+  validates :name, uniqueness: true
+  validates :name, presence: true
+end
